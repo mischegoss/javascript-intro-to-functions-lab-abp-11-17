640c82b89a9f17c28e12c124c5fce86f269ce8df
mischegoss <mischegoss@gmail.com>
2017-11-15 20:41:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 function shout(string) {
   return string.toUpperCase();
+
+function whisper(string) {
+  return string.toLowerCase();
+
+function whisper(string) {
+  console.log (string.spyOn());
+
+
+  
+
+    
   
